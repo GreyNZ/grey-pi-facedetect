40c91416ac52b547789c323d8fe090f6a48657b6
Haruka Ichinose <Haruka.Ichinose@umajin.com>
2023-04-24 21:15:32 +1200

    Streamed video from camera.

--- src/magic_window/magic_window.py
+++ src/magic_window/magic_window.py
@@ -14,6 +14,8 @@ class MagicWindow:
         self.display_size = display_size
         self.current_vid_frame = None
         self.display_bkg_frame = None
+        self.camera_res = None
+        self.camera_fps = None
 
     def _set_background_image(self, background_image: str) -> cv2.Mat:
         assert (os.path.isfile(background_image)), f'File {background_image=} does not exist'
@@ -33,11 +35,39 @@ class MagicWindow:
             cv2.imshow("Window To Another World!", self.background_image)
             cv2.waitKey(0)
             sys.exit()
-
         cv2.destroyAllWindows()
+
+    def stream_video(self):
+        try:
+            cap = cv2.VideoCapture(0)
+        except Exception as e:
+            print(f'Failed to load camera {e=}')
+
+        while True:
+            ret, frame = cap.read(0)
+            if (self.camera_res is None):
+                self.camera_res = np.shape(frame)
+                self._set_fps(cap)
+            cv2.imshow('Video camera', frame)
+        
+            if cv2.waitKey(1) & 0xFF == ord('q'):
+                break
+        cap.release()
+        cv2.destroyAllWindows()
+
+    def _set_fps(self, cap):
+        # https://learnopencv.com/how-to-find-frame-rate-or-frames-per-second-fps-in-opencv-python-cpp/
+        (major_ver, minor_ver, subminor_ver) = (cv2.__version__).split('.')
+        if int(major_ver) < 3:
+            self.camera_fps = cap.get(cv2.cv.CV_CAP_PROP_FPS)
+            print("Frames per second using video.get(cv2.cv.CV_CAP_PROP_FPS): {0}".format(self.camera_fps))
+        else:
+            self.camera_fps = cap.get(cv2.CAP_PROP_FPS)
+            print("Frames per second using video.get(cv2.CAP_PROP_FPS) : {0}".format(self.camera_fps))
     
 
 
 magic_window = MagicWindow('src\magic_window\hobbiton.jpeg', (500, 500))
-magic_window.printer()
+# magic_window.printer()
+magic_window.stream_video()
 
