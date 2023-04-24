4b66c9e132c303c820b31a91790f986a1e08e961
Haruka Ichinose <Haruka.Ichinose@umajin.com>
2023-04-24 21:55:39 +1200

    Detected multiple faces in camera feed.

--- src/magic_window/helpers.py
+++ src/magic_window/helpers.py
@@ -1,3 +1,10 @@
+import cv2
+import os
+
+FILE_NAME = 'src/magic_window/haarcascade_frontalface_default.xml'
+assert (os.path.isfile(FILE_NAME))
+FACE_CASCADE = cv2.CascadeClassifier(FILE_NAME)
+
 def return_largest(a,b):
     """Compare a,b and reutrn largest"""
     
@@ -11,4 +18,16 @@ def return_largest(a,b):
     else:
         return b
     
+def adj_detect_face(img):
+    """Dectect faces in an image with no overlaps
+    Arg img: an image
     
+    Result: 
+    face_img = image with bounding boxes
+    face-rects = array of arrays, each array is corners of face bounding box
+    """
+    face_img = img.copy()
+    face_rects = FACE_CASCADE.detectMultiScale(face_img,scaleFactor=1.2, minNeighbors=5) 
+    for (x,y,w,h) in face_rects: 
+        cv2.rectangle(face_img, (x,y), (x+w,y+h), (255,255,255), 10) 
+    return face_img, face_rects
--- src/magic_window/magic_window.py
+++ src/magic_window/magic_window.py
@@ -3,9 +3,8 @@ import cv2
 import matplotlib.pyplot as plt
 import os
 import sys
+from helpers import adj_detect_face
 # %matplotlib inline
-
-# face_cascade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
 
 class MagicWindow:
 
@@ -16,6 +15,7 @@ class MagicWindow:
         self.display_bkg_frame = None
         self.camera_res = None
         self.camera_fps = None
+        self.background_center = None
 
     def _set_background_image(self, background_image: str) -> cv2.Mat:
         assert (os.path.isfile(background_image)), f'File {background_image=} does not exist'
@@ -25,10 +25,13 @@ class MagicWindow:
             return img
         except Exception as e:
             print(f'Failed to load image due to {e=}')
-            
 
-    def get_background_center(self) -> tuple[int, int]:
+    def _set_background_center(self):
         '''Find the center pixel coordinate of the background image'''
+        x, y, _ = self.background_image.shape
+        half_x, half_y = x // 2, y // 2
+        print(f'Background center pixel: ({half_x},{half_y})')
+        self.background_center = (half_x, half_y)
 
     def printer(self):
         while True:
@@ -44,11 +47,18 @@ class MagicWindow:
             print(f'Failed to load camera {e=}')
 
         while True:
-            ret, frame = cap.read(0)
+            ret, self.current_vid_frame = cap.read(0)
             if (self.camera_res is None):
-                self.camera_res = np.shape(frame)
+                self.camera_res = np.shape(self.current_vid_frame)
                 self._set_fps(cap)
-            cv2.imshow('Video camera', frame)
+            # cv2.imshow('Video camera', frame)   # Uncomment me to show the video feed
+
+            # Process video frame to extract center pixel of face
+            face_img, face_rects = self.detect_face(adj_detect_face)
+            cv2.imshow('Face image', face_img)
+
+            # Transpose center pixel onto background and slice window
+            # Display window
         
             if cv2.waitKey(1) & 0xFF == ord('q'):
                 break
@@ -65,9 +75,14 @@ class MagicWindow:
             self.camera_fps = cap.get(cv2.CAP_PROP_FPS)
             print("Frames per second using video.get(cv2.CAP_PROP_FPS) : {0}".format(self.camera_fps))
     
+    def detect_face(self, detecter):
+        face_img, face_rects = detecter(self.current_vid_frame)
+        return face_img, face_rects
+
 
 
 magic_window = MagicWindow('src\magic_window\hobbiton.jpeg', (500, 500))
 # magic_window.printer()
+# magic_window.stream_video()
+# magic_window._set_background_center()
 magic_window.stream_video()
-
